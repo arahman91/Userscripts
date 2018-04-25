8b7cbc097823a048aba90b90c3649ce9448c19a1
arahman91 <arahman56@gmail.com>
2018-04-25 00:26:53 -0400

    Create RedditTitle.user.js

---
+++ RedditTitle.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        Change Reddit Title
+// @description Sets Basic Reddit Title
+// @match       *://*.reddit.com/*
+// @version     3.14
+// @grant       none
+// @run-at      document-start
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+     document.title = "Reddit: The front page of the internet"
+})();
